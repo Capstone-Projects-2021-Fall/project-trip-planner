f013ea1eed0c3f27314d04532af597c6049a59b6
garymejia <fjgmejia@gmail.com>
2021-10-31 18:16:26 -0400

    modified lambda userAcount function

--- AWSLambdas/userAccount/function_handler.py
+++ AWSLambdas/userAccount/function_handler.py
@@ -14,16 +14,37 @@ connection = pymysql.connect(endpoint, user=username, passwd=password, db=databa
 def lambda_handler(event, context):
     cursor = connection.cursor()
     http_method = event["requestContext"]["http"]["method"]
-
+    #http_method = event["httpMethod"]
     if http_method == 'POST':
         body = json.loads(event['body'])
         username = body["username"]
+        firstName = body["firstName"]
+        lastName = body['lastName']
+        DOB = parser.parse(body['DOB']).date().isoformat()
         email = body["email"]
         password = body["password"]
-        args = [username, email, password, True]
+        args = [username, firstName, lastName, DOB, email, password, 0, ""]
         cursor.callproc('CreateUserNormalLogin', args)
         connection.commit()
         return {
                 'statusCode': 200,
                 'body': json.dumps(args)
-        }  
+        }
+    """elif http_method == 'GET':
+        email:str = "bobbyhill@gmail.com"
+        password:str = "ilikepropane"
+        args = [email, password, None, 0, ""]
+        cursor.callproc('TryGetLoginID', args)
+        connection.close()
+        userJson = { }
+        userJson["UserID"] = args[2]
+        userJson["ErrorCode"] = args[3]
+        userJson["ErrorMessage"] = args[4]
+        #no idea if this is how it works plz fix if not
+        return {
+            "statusCode": 200,
+            "body": json.dumps(userJson, default=str),
+            "headers": {
+                "Access-Control-Allow-Headers":"*",
+                "Accept":"json"
+        }"""
