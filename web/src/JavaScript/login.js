aebae9e101ff45db06d662cf806ad207b06c82c6
garymejia <fjgmejia@gmail.com>
2021-11-07 13:42:04 -0500

    login api adjustments

--- web/src/JavaScript/login.js
+++ web/src/JavaScript/login.js
@@ -1,20 +1,16 @@
-    var callAPI = (username, password)=>{
+    var callAPI = (email, password)=>{
         // instantiate a headers object
         var myHeaders = new Headers();
         // add content type header to object
         myHeaders.append("Content-Type", "application/json");
         // using built in JSON utility package turn object to string and store in a variable
-        var raw = JSON.stringify({"username":username, "password": password});
+        var raw = JSON.stringify({"email":email, "password": password});
 
         // create a JSON object with parameters for API call and store in a variable
         var requestOptions = {
-            method: 'GET',
+            method:'POST',
+            body:raw
         };
-        console.log(username, password);
         // make API call with parameters and use promises to get response
-        fetch("https://hhd3reswr9.execute-api.us-west-2.amazonaws.com/UserManagement?username=" + username + "&password=" + password, requestOptions).then(response => response.text())
-      .then( function(data){
-          let value = JSON.parse(data);
-          console.log(value);
-      });
+        fetch("https://hhd3reswr9.execute-api.us-west-2.amazonaws.com/NormalUserLogin", requestOptions);
     }
