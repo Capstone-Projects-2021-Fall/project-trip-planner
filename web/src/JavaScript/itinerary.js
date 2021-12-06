0ece170376eb29a1491dc18590c496a5a4b6aabd
garymejia <fjgmejia@gmail.com>
2021-12-06 00:31:20 -0500

    possibly formatting issue

--- web/src/JavaScript/itinerary.js
+++ web/src/JavaScript/itinerary.js
@@ -1692,17 +1692,17 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
 	  })
 	  .catch((e) => window.alert("Directions request failed due to " + status));
 
-	  var axios = require('axios');
 
-		var config = {
-			method: 'get',
-			url: 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + startAddress + 'destinations=' + endAddress + '&key=' + 'AIzaSyAGbze5amXmflZVecAENrKG2sC-r_kecKY',
-			headers: { }
+	  var config = {
+		method: 'GET', mode: "no-cors",
 		};
 
-		axios(config)
-		.then(function (response) {
-		console.log(JSON.stringify(response.data));
+		fetch('https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + startAddress + 'destinations=' + endAddress + '&key=' + 'AIzaSyAGbze5amXmflZVecAENrKG2sC-r_kecKY', config).then(function (response) {
+		
+		//below is url from googles example
+		//fetch('https://maps.googleapis.com/maps/api/distancematrix/json?origins=40.6655101%2C-73.89188969999998&destinations=40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626&key=' + 'AIzaSyAGbze5amXmflZVecAENrKG2sC-r_kecKY', config).then(function (response) {
+
+		console.log(JSON.stringify(response));
 		})
 		.catch(function (error) {
 		console.log(error);
